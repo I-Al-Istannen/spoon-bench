3e4f39e1ca5df53fe3e4304696a99d8a4fbe9f97
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2022-08-30 19:39:39 +0200

    Initial commit

---
+++ common.py
@@ -0,0 +1,89 @@
+
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+import shutil
+from subprocess import STDOUT, check_output
+import sys
+from typing import Any, Union
+
+
+DIM = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+BLUE = "\033[34m"
+CYAN = "\033[36m"
+
+BOLD = "\033[1m"
+RESET = "\033[0m"
+
+
+def print_header(header: Any, end: str = "\n"):
+    print("\n" + BOLD + CYAN + str(header) + RESET, end=end, file=sys.stderr)
+
+
+def print_info(info: Any, end: str = "\n"):
+    print(BLUE + str(info + RESET), end=end, file=sys.stderr)
+
+
+def print_success(message: Any, end: str = "\n"):
+    print(GREEN + str(message) + RESET, end=end, file=sys.stderr)
+
+
+class Interpretation(str, Enum):
+    LESS_IS_BETTER = "LESS_IS_BETTER"
+    MORE_IS_BETTER = "MORE_IS_BETTER"
+    NEUTRAL = "NEUTRAL"
+
+
+@dataclass
+class SuccessMeasurement:
+    unit: str
+    interpretation: Interpretation
+    values: list[float]
+
+    def to_dict(self):
+        return self.__dict__
+
+
+@dataclass
+class ErrorMeasurement:
+    unit: str
+    interpretation: Interpretation
+    error: str
+
+    def to_dict(self):
+        return self.__dict__
+
+
+Measurement = Union[SuccessMeasurement, ErrorMeasurement]
+
+
+@dataclass
+class Benchmark:
+    name: str
+    measurements: dict[str, Measurement]
+
+    def to_dict(self):
+        vals = dict()
+        for name, measurement in self.measurements.items():
+            vals[name] = measurement.to_dict()
+
+        return {"name": self.name, "measurements": vals}
+
+
+def clone_guava() -> Path:
+    print_info("Cloning guava")
+    guava_path = Path("/tmp/guava")
+
+    if guava_path.exists():
+        shutil.rmtree(guava_path)
+
+    check_output(
+        args=["git", "clone", "https://github.com/google/guava", str(guava_path), "--depth=1"],
+        stderr=STDOUT
+    )
+
+    print_success("  Cloned")
+
+    return guava_path
---
+++ javadoc_api.py
@@ -0,0 +1,132 @@
+
+import json
+import shutil
+from pathlib import Path
+from subprocess import STDOUT, check_output
+
+from common import (Benchmark, Interpretation, Measurement, SuccessMeasurement, clone_guava,
+                    print_header, print_info, print_success)
+
+
+def clone_javadoc_api() -> Path:
+    print_info("Cloning JavadocApi")
+    directory = Path("/tmp/JavadocApi")
+    if directory.exists():
+        shutil.rmtree(directory)
+
+    check_output(
+        [
+            "git",
+            "clone",
+            "https://github.com/I-Al-Istannen/JavadocApi",
+            str(directory)
+        ],
+        stderr=STDOUT
+    )
+    print_success("  Cloning finished")
+
+    return directory
+
+
+def write_javadoc_api_config(javadoc_api_path: Path, path_to_index: Path) -> Path:
+    text = """
+    {
+      "outputPath": "",
+      "resourcePaths": [
+        "{{PATH}}"
+      ],
+      "allowedPackages": [
+        "*"
+      ],
+      "outputTimings": true
+    }
+    """.replace("{{PATH}}", str(path_to_index.absolute()))
+
+    path = javadoc_api_path / "javadocapi-config.json"
+    with open(path, "w") as file:
+        file.write(text)
+
+    return path
+
+
+def patch_javadoc_api(javadoc_api_path: Path, spoon_jar: Path):
+    with open("JavadocApi.patch.template", "r") as file:
+        text = file.read().replace("{{PATH}}", str(spoon_jar))
+
+    with open(javadoc_api_path / "JavadocApi.patch", "w") as file:
+        file.write(text)
+
+    print_info("Patching javadoc api...")
+    check_output(
+        args=["patch < JavadocApi.patch"],
+        shell=True,
+        cwd=javadoc_api_path,
+        stderr=STDOUT
+    )
+    print_success("  Patched")
+
+
+def build_javadoc_api(javadoc_api_path: Path):
+    print_info("Building JavadocApi")
+    check_output(
+        args=["mvn", "clean", "package"],
+        cwd=javadoc_api_path,
+        env={"JAVA_HOME": "/home/bench/.sdkman/candidates/java/current"},
+        stderr=STDOUT
+    )
+    print_success("  Built")
+
+
+def run_javadoc_api_iteration(spoon_jar: Path, javadoc_api_path: Path, config_path: Path) -> dict[str, float]:
+    javadocapi_jar = javadoc_api_path / "target/JavadocApi.jar"
+
+    output = check_output(
+        args=[
+            "/home/bench/.sdkman/candidates/java/current/bin/java",
+            "-cp",
+            f"{str(spoon_jar.absolute())}:" +
+            str(javadocapi_jar.absolute()),
+            "de.ialistannen.javadocapi.indexing.Indexer",
+            str(config_path.absolute())
+        ],
+        cwd=javadoc_api_path,
+        stderr=STDOUT
+    ).decode()
+    print_success("  Run finished")
+
+    return json.loads([x for x in output.splitlines() if x.startswith('{"')][0])
+
+
+def run_javadoc_api(spoon_jar: Path, javadoc_api_path: Path, config_path: Path) -> Benchmark:
+    print_info("Running JavadocApi")
+
+    data: dict[str, list[float]] = dict()
+    for _ in range(10):
+        for name, val in run_javadoc_api_iteration(spoon_jar, javadoc_api_path, config_path).items():
+            if name in data:
+                data[name].append(val)
+            else:
+                data[name] = [val]
+
+    measurements: dict[str, Measurement] = dict()
+    for name, duration in data.items():
+        measurements[name] = SuccessMeasurement(
+            unit="ms",
+            interpretation=Interpretation.LESS_IS_BETTER,
+            values=duration
+        )
+
+    return Benchmark(name="JavadocApi", measurements=measurements)
+
+
+def measure_javadoc_api(spoon_jar: Path):
+    print_header("Running JavadocApi")
+    javadoc_api = clone_javadoc_api()
+    guava_path = clone_guava() / "guava/src"
+
+    patch_javadoc_api(javadoc_api, spoon_jar)
+    build_javadoc_api(javadoc_api)
+
+    config = write_javadoc_api_config(javadoc_api, guava_path)
+
+    return run_javadoc_api(spoon_jar, javadoc_api, config)
